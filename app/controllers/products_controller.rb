0e4736bb25961b5112f78b2f55b5da78cf177c32
rovinsanque0 <rovinsanque0@gmail.com>
2025-10-06 10:42:56 -0500

    updated products_controller. Renamed csv file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.includes(:category).all 
+    @products = Product.includes(:category).all
   end
   def show
     @product = Product.find(params[:id])
